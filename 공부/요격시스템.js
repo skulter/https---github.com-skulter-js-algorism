13c6a1acc1abae25079bd5796290c608a68c275e
jinho kim <jinho.kim@cloudmt.co.kr>
2023-11-21 02:25:22 +0900

    요격시스템 어렵다..

--- 공부/요격시스템.js
+++ 공부/요격시스템.js
@@ -1,7 +1,7 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/181188
 // 설명 https://chamdom.blog/pg2-181188/
 // [[4,5],[4,8],[10,14],[11,13],[5,12],[3,7],[1,4]] // 3
-function solution(targets) {
+function bestSolution(targets) {
   let answer = 1;
   // 1. 시작점을 기준으로 내림차순
   targets.sort((a, b) => b[0] - a[0]);
@@ -18,7 +18,7 @@ function solution(targets) {
 }
 
 console.log(
-  solution([
+  bestSolution([
     [4, 5],
     [4, 8],
     [10, 14],
@@ -26,5 +26,48 @@ console.log(
     [5, 12],
     [3, 7],
     [1, 4],
+  ])
+);
+
+function solution(targets) {
+  let fireCount = 1;
+  targets.sort((a, b) => b[0] - a[0]);
+  let s = targets[0][0];
+  for (const target of targets) {
+    const [currentS, currentE] = target;
+    if (s >= currentE) {
+      fireCount += 1;
+      s = currentS;
+    }
+  }
+  return fireCount;
+}
+
+function solution2(targets) {
+  let fireCount = 1;
+  targets.sort((a, b) => a[0] - b[0]);
+  console.log(targets);
+  let [s, e] = targets.shift();
+
+  for (const target of targets) {
+    const [currentS, currentE] = target;
+    if (currentS >= e) {
+      fireCount += 1;
+      e = currentE;
+    }
+  }
+
+  return fireCount;
+}
+console.log(
+  solution2([
+    [1, 2],
+    [2, 3],
+    [0, 99999],
+  ]),
+  solution([
+    [1, 2],
+    [2, 3],
+    [0, 99999],
   ])
 );
